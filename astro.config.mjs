76e2a496a1051ca945da9ad9bff878e596700150
Matthew Dzwil <mattdzwil@gmail.com>
2026-02-18 22:24:16 -0500

    Changes needed to deploy on GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://diz138.github.io',
+  base: 'apex-dashboard',
+});
